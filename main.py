3ecf738317a4cd4058b2496dc89b2e911e0d09fc
Yuhang Li <18326007+WilliamYuhangLee@users.noreply.github.com>
2019-01-26 17:49:09 -0500

    Add SQLAlchemy support, db models

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
-import sys
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 APP_NAME = "Commentaria"
 
 app = Flask(APP_NAME)
 
 app.config["SECRET_KEY"] = "835c9d467dfb46b00af5a442c8c4a90f"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://localhost/commentaria"
+
+db = SQLAlchemy(app)
+
+
+# Data models
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    profile_picture = db.Column(db.String(20), nullable=False, default="default.jpg")
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship("posts", backref="author", lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.profile_picture}')"
+
+
+class Post(db.Model):
+    __tablename__ = "posts"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
 
 posts = [
     {
