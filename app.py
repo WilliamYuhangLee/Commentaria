b04f952f3f2fffeef2f76838f8bb08a4b08b3f55
Yuhang Li <18326007+WilliamYuhangLee@users.noreply.github.com>
2019-01-25 23:39:14 -0500

    Initial setup

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+posts = [
+    {
+        "author": "William Lee",
+        "title": "First Post",
+        "content": "First post!",
+        "date_posted": "Jan 1, 2018"
+    },
+    {
+        "author": "William Lee",
+        "title": "Second Post",
+        "content": "Second post!",
+        "date_posted": "Jan 2, 2018"
+    }
+]
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html", posts=posts)
 
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
