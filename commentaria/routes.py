451a04726c6782994a94a3bdab0a13fc9d5d2819
Yuhang Li <18326007+WilliamYuhangLee@users.noreply.github.com>
2019-01-27 03:38:59 -0500

    Implemented post deletion

--- commentaria/routes.py
+++ commentaria/routes.py
@@ -144,3 +144,15 @@ def edit_post(post_id):
         form.title.data = post.title
         form.content.data = post.content
         return render_template("edit_post.html", title="Edit Post", form=form)
+
+
+@app.route("/post/<int:post_id>/delete", methods=["POST"])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash("Your post has been delete!", category="success")
+    return redirect(url_for("home"))
