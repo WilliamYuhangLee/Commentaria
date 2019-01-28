0b0d09cbe25836282dc5929c2015babf9427dd8f
Yuhang Li <18326007+WilliamYuhangLee@users.noreply.github.com>
2019-01-28 04:56:02 -0500

    Edited time display: add locale, from now

--- commentaria/__init__.py
+++ commentaria/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_moment import Moment
 
 from commentaria.config import Config
 
@@ -12,6 +13,7 @@ login_manager = LoginManager()
 login_manager.login_view = "users.login"  # which function to login
 login_manager.login_message_category = "info"  # set login message style (Bootstrap)
 mail = Mail()
+moment = Moment()
 
 
 def create_app(config_class=Config):
@@ -22,6 +24,7 @@ def create_app(config_class=Config):
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
+    moment.init_app(app)
 
     from commentaria.users.routes import users
     from commentaria.posts.routes import posts
